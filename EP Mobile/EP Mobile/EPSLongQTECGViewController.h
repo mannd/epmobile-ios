d7820dd5048fdd1872ad24b08cec592c6842d897
David Mann <mannd@epstudiossoftware.com>
2012-08-15 19:46:18 -0400

    added Long QT ECG Patterns

---
+++ EP Mobile/EP Mobile/EPSLongQTECGViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPSLongQTECGViewController.h
+//  EP Mobile
+//
+//  Created by David Mann on 8/15/12.
+//  Copyright (c) 2012 EP Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSLongQTECGViewController : UIViewController
+
+@end
