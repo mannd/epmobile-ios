e6b0256ae766cbb6e24f2bdfa62f51dd4bf258bc
David Mann <mannd@epstudiossoftware.com>
2012-08-18 22:16:08 -0400

    completed LQTS subtypes

---
+++ EP Mobile/EP Mobile/EPSLQTSDetailsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPSLQTSDetailsTableViewController.h
+//  EP Mobile
+//
+//  Created by David Mann on 8/16/12.
+//  Copyright (c) 2012 EP Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSLQTSDetailsTableViewController : UITableViewController
+
+@end
