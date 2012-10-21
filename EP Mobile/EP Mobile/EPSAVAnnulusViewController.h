477b116988601e237f119ae76efeb3a1c1a90176
David Mann <mannd@epstudiossoftware.com>
2012-10-21 19:36:25 -0400

    all code for v1.0 done

--- EP Mobile/EP Mobile/EPSAVAnnulusViewController.h
+++ EP Mobile/EP Mobile/EPSAVAnnulusViewController.h
@@ -14,6 +14,16 @@
 @property (strong, nonatomic) IBOutlet UIImageView *epicardialapImageView;
 @property (strong, nonatomic) IBOutlet UIImageView *lalapImageView;
 @property (strong, nonatomic) IBOutlet UIImageView *llapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *lpapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *lplapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *msapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *psmaapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *pstaapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *raapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *ralapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *rlapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *rplapImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *rpapImageView;
 
 @property (strong, nonatomic) IBOutlet UILabel *mapLocationLabel;
 
--- EP Mobile/EP Mobile/EPSAVAnnulusViewController.m
+++ EP Mobile/EP Mobile/EPSAVAnnulusViewController.m
@@ -20,6 +20,16 @@
 @synthesize epicardialapImageView;
 @synthesize lalapImageView;
 @synthesize llapImageView;
+@synthesize lpapImageView;
+@synthesize lplapImageView;
+@synthesize msapImageView;
+@synthesize psmaapImageView;
+@synthesize pstaapImageView;
+@synthesize raapImageView;
+@synthesize ralapImageView;
+@synthesize rlapImageView;
+@synthesize rplapImageView;
+@synthesize rpapImageView;
 @synthesize showPathway;
 @synthesize message;
 @synthesize location1;
@@ -46,13 +56,18 @@
         self.epicardialapImageView.hidden = !([self.location1 isEqualToString:SUBEPI] || [self.location2 isEqualToString:SUBEPI]);
         self.lalapImageView.hidden = !([self.location1 isEqualToString:LAL] || [self.location2 isEqualToString:LAL]);
         self.llapImageView.hidden = !([self.location1 isEqualToString:LL] || [self.location2 isEqualToString:LL]);
-        
+        self.lpapImageView.hidden = !([self.location1 isEqualToString:LP] || [self.location2 isEqualToString:LP]);
+        self.lplapImageView.hidden = !([self.location1 isEqualToString:LPL] || [self.location2 isEqualToString:LPL]);
+        self.msapImageView.hidden = !([self.location1 isEqualToString:MSTA] || [self.location2 isEqualToString:MSTA]);
+        self.psmaapImageView.hidden = !([self.location1 isEqualToString:PSMA] || [self.location2 isEqualToString:PSMA]);
+        self.pstaapImageView.hidden = !([self.location1  isEqualToString:PSTA] || [self.location2 isEqualToString:PSTA]);
+        self.raapImageView.hidden = !([self.location1 isEqualToString:RA] || [self.location2 isEqualToString:RA]);
+        self.ralapImageView.hidden = !([self.location1 isEqualToString:RAL] || [self.location2 isEqualToString:RAL]);
+        self.rlapImageView.hidden = !([self.location1 isEqualToString:RL] || [self.location2 isEqualToString:RL]);
+        self.rplapImageView.hidden = !([self.location1 isEqualToString:RPL] || [self.location2 isEqualToString:RPL]);
+        self.rpapImageView.hidden = !([self.location1 isEqualToString:RP] || [self.location2 isEqualToString:RP]);
     }
-//    UIImage *testView = [UIImage imageNamed:@"asap.png"];
-//    UIImageView *overlayImageView = [[UIImageView alloc] initWithImage:testView];
-//
-//    [self.mapImageView addSubview:overlayImageView];
-    
+
 }
 
 - (void)viewDidUnload
@@ -66,6 +81,16 @@
     [self setLalapImageView:nil];
     [self setLalapImageView:nil];
     [self setLlapImageView:nil];
+    [self setLpapImageView:nil];
+    [self setLplapImageView:nil];
+    [self setMsapImageView:nil];
+    [self setPsmaapImageView:nil];
+    [self setPstaapImageView:nil];
+    [self setRaapImageView:nil];
+    [self setRalapImageView:nil];
+    [self setRlapImageView:nil];
+    [self setRplapImageView:nil];
+    [self setRpapImageView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
