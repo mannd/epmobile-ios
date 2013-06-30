9dc45fabf0de468721ceac4071bed5064819109e
David Mann <mannd@epstudiossoftware.com>
2013-06-30 19:55:51 -0400

    Weight calculator functionally similar to current Android version

--- EP Mobile/EP Mobile/EPSWeightCalculatorViewController.h
+++ EP Mobile/EP Mobile/EPSWeightCalculatorViewController.h
@@ -9,19 +9,26 @@
 #import <UIKit/UIKit.h>
 
 @interface EPSWeightCalculatorViewController : UIViewController
-@property (strong, nonatomic) IBOutlet UISegmentedControl *sexSegementedControl;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *sexSegmentedControl;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *weightUnitsSegmentedControl;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *heightUnitsSegmentedControl;
 @property (strong, nonatomic) IBOutlet UITextField *weightTextField;
 @property (strong, nonatomic) IBOutlet UITextField *heightTextField;
-@property (strong, nonatomic) IBOutlet UILabel *ibwLabel;
-@property (strong, nonatomic) IBOutlet UILabel *abwLabel;
-@property (strong, nonatomic) IBOutlet UILabel *ibwResultLabel;
-@property (strong, nonatomic) IBOutlet UILabel *abwResultLabel;
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property (strong, nonatomic) NSString *defaultWeightUnit;
+@property (strong, nonatomic) NSString *defaultHeightUnit;
+
 - (IBAction)calculate:(id)sender;
 - (IBAction)clear:(id)sender;
-
 - (IBAction)copyIbw:(id)sender;
-
 - (IBAction)copyAbw:(id)sender;
+- (IBAction)toggleWeightUnits:(id)sender;
+- (IBAction)toggleHeightUnits:(id)sender;
+- (IBAction)toggleSex:(id)sender;
+- (IBAction)textFieldDoneEditing:(id)sender;
+- (IBAction)backgroundTap:(id)sender;
+
+
+
 @end
