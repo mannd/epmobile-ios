48a5d616ff279e2db64967a72cd65ee3ed0201ea
David Mann <mannd@epstudiossoftware.com>
2013-08-07 20:22:43 -0400

    added syncope risk scores table view

---
+++ EP Mobile/EP Mobile/EPSSyncopeRiskViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPSSyncopeRiskViewController.h
+//  EP Mobile
+//
+//  Created by David Mann on 8/7/13.
+//  Copyright (c) 2013 EP Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSSyncopeRiskViewController : UITableViewController
+
+@end
---
+++ EP Mobile/EP Mobile/EPSSyncopeRiskViewController.m
@@ -0,0 +1,43 @@
+//
+//  EPSSyncopeRiskViewController.m
+//  EP Mobile
+//
+//  Created by David Mann on 8/7/13.
+//  Copyright (c) 2013 EP Studios. All rights reserved.
+//
+
+#import "EPSSyncopeRiskViewController.h"
+
+@interface EPSSyncopeRiskViewController ()
+
+@end
+
+@implementation EPSSyncopeRiskViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
