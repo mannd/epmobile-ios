0615a263c0c2be1e6abae25e09f22dcd4c334cd4
David Mann <mannd@epstudiossoftware.com>
2015-05-30 22:20:26 +0200

    EPSHcmScd2014 gives bare result and error messages.  Will merge back into develop branch.

--- EP Mobile/EP Mobile/EPSHcmScd2014ViewController.m
+++ EP Mobile/EP Mobile/EPSHcmScd2014ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "EPSHcmScd2014ViewController.h"
+#import "EPSLogging.h"
 
 @interface EPSHcmScd2014ViewController ()
 
@@ -14,10 +15,21 @@
 
 @implementation EPSHcmScd2014ViewController
 
+static const int NO_ERROR = 8999;
+static const int NUMBER_EXCEPTION = 9000;
+static const int AGE_OUT_OF_RANGE = 9001;
+static const int THICKNESS_OUT_OF_RANGE = 9002;
+static const int GRADIENT_OUT_OF_RANGE = 9003;
+static const int SIZE_OUT_OF_RANGE = 9004;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.navigationItem.title = @"HCM SCD 2014";
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeInfoLight];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:btn];
+    [btn addTarget:self action:@selector(showNotes) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,6 +48,10 @@
     [self.gradientTextField resignFirstResponder];
 }
 
+- (void)showNotes {
+    // TODO
+}
+
 /*
 #pragma mark - Navigation
 
@@ -47,6 +63,89 @@
 */
 
 - (IBAction)calculate:(id)sender {
+    int errorCode = NO_ERROR;
+    NSInteger age = [self textFieldToInt:self.ageTextField];
+    NSInteger thickness = [self textFieldToInt:self.thicknessTextField];
+    NSInteger size = [self textFieldToInt:self.sizeTextField];
+    NSInteger gradient = [self textFieldToInt:self.gradientTextField];
+    // TODO user will be confused if zero gradient entered, and error message isn't clear
+    // that the range is 2 to whatever... What to do??
+    // Maybe put range in hint
+    BOOL hasFamilyHxScd = self.familyHxSwitch.on;
+    BOOL hasNsvt = self.nsvtSwitch.on;
+    BOOL hasSyncope = self.syncopeSwitch.on;
+    if (age <= 0 || thickness <= 0 || size <= 0 || gradient <= 0) {
+        errorCode = NUMBER_EXCEPTION;
+    }
+    else if (age > 115 || age < 16) {
+        errorCode = AGE_OUT_OF_RANGE;
+    }
+    else if (thickness < 10 || thickness > 35) {
+        errorCode = THICKNESS_OUT_OF_RANGE;
+    }
+    else if (size < 28 || size > 67) {
+        errorCode = SIZE_OUT_OF_RANGE;
+    }
+    else if (gradient < 2 || gradient > 154) {
+        errorCode = GRADIENT_OUT_OF_RANGE;
+    }
+    if (errorCode != NO_ERROR) {
+        [self showErrorDialog:errorCode];
+        return;
+    }
+    double coefficient = 0.998;
+    double prognosticIndex = 0.15939858 * thickness
+    - 0.00294271 * thickness * thickness
+    + 0.0259082 * size
+    + 0.00446131 * gradient
+    + (hasFamilyHxScd ? 0.4583082 : 0.0)
+    + (hasNsvt ? 0.82639195 : 0.0)
+    + (hasSyncope ? 0.71650361 : 0.0)
+    - 0.01799934 * age;
+    double scdProb = 1 - pow(coefficient, exp(prognosticIndex));
+    EPSLog(@"scdProb = %f", scdProb);
+    [self showResultDialog:scdProb];
+}
+
+- (NSInteger)textFieldToInt:(UITextField *)textField {
+    return [textField.text integerValue];
+}
+
+- (void)showErrorDialog:(NSInteger)errorCode {
+    NSString *title = @"Error";
+    NSString *message;
+    switch (errorCode) {
+        case NUMBER_EXCEPTION:
+            message = @"One or more entries are invalid.";
+            break;
+        case AGE_OUT_OF_RANGE:
+            message = @"Age must be between 16 and 115 years.";
+            break;
+        case THICKNESS_OUT_OF_RANGE:
+            message = @"Maximum LV wall thickness must be between 10 and 35 mm.";
+            break;
+        case SIZE_OUT_OF_RANGE:
+            message = @"Maximum LA diameter must be between 28 and 67 mm.";
+            break;
+        case GRADIENT_OUT_OF_RANGE:
+            message = @"Maximum LV outflow tract gradient must between 2 and 154 mmHg.";
+            break;
+        case NO_ERROR:  // fall through, and shouldn't be here anyway
+        default:
+            return;
+    }
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
+}
+
+- (void)showResultDialog:(double)prob {
+    // make it a percentage
+    prob = prob * 100.0;
+    NSString *title = @"HCM-SCD Risk";
+    NSString *message = [NSString stringWithFormat:@"5 year SCD Risk = %2.2f%%", prob];
+    // TODO add ICD recommendation to message
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alertView show];
 }
 
 - (IBAction)clear:(id)sender {
