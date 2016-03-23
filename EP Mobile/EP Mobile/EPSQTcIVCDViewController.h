11ba45a0d19876f376d3dcdd591e3edc3595d76d
David Mann <mannd@epstudiossoftware.com>
2016-03-23 22:03:21 +0100

    Layout QTcIVCD view

---
+++ EP Mobile/EP Mobile/EPSQTcIVCDViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPSQTcIVCDViewController.h
+//  EP Mobile
+//
+//  Created by David Mann on 3/23/16.
+//  Copyright © 2016 EP Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPSQTcIVCDViewController : UIViewController
+
+@end
