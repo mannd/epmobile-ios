d5291998368e9cf4684f649dea7c4752f41c37a0
David Mann <mannd@epstudiossoftware.com>
2018-02-22 11:00:57 -0700

    Pre-update to CMS 2018 ICD guidelines

---
+++ EP Mobile/EP Mobile/EPSCMSModel.m
@@ -0,0 +1,13 @@
+//
+//  EPSCMSModel.m
+//  EP Mobile
+//
+//  Created by David Mann on 2/22/18.
+//  Copyright © 2018 EP Studios. All rights reserved.
+//
+
+#import "EPSCMSModel.h"
+
+@implementation EPSCMSModel
+
+@end
