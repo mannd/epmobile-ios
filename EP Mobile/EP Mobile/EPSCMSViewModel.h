302af83f27e65fc85022fd17ebc4aa495068fb8a
David Mann <mannd@epstudiossoftware.com>
2018-02-25 09:55:44 -0700

    EPSCMSModel passes tests

---
+++ EP Mobile/EP Mobile/EPSCMSViewModel.h
@@ -0,0 +1,13 @@
+//
+//  EPSCMSViewModel.h
+//  EP Mobile
+//
+//  Created by David Mann on 2/22/18.
+//  Copyright © 2018 EP Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EPSCMSViewModel : NSObject
+
+@end
--- EP Mobile/EP MobileTests/EP_MobileTests.m
+++ EP Mobile/EP MobileTests/EP_MobileTests.m
@@ -16,6 +16,7 @@
 #import "EPSRiskFactor.h"
 #import "EPSChadsRiskScore.h"
 #import "EPSQTMethods.h"
+#import "EPSCMSModel.h"
 
 #define MESSAGE_L @"Actual result was %ld"
 
@@ -151,5 +152,179 @@
     XCTAssertTrue(result == 421, MESSAGE_L, result);
 }
 
+- (void)testCMSModel {
+    EPSCMSModel *model = [[EPSCMSModel alloc] init];
+    struct Result result = [model getResult];
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(result.indication == IndicationNone);
+    XCTAssert(result.details == NoIndications);
+    XCTAssert(result.needsSdmEncounter == NO);
+    [self absoluteExclusionTests:model];
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(result.details == NoIndications);
+    model.cardiacArrest = YES;
+    result = [model getResult];
+    // problem now is no ef
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(result.details == NoEF);
+    // Absolute exclusions still override No EF and No Indications so test it again
+    [self absoluteExclusionTests:model];
+    // try ting something else with no EF
+    model.cardiacArrest = NO;
+    model.icdAtEri = YES;
+    result = [model getResult];
+    XCTAssert(result.details == NoEF);
+    // re model
+    model = [self newModel];
+    result = [model getResult];
+    XCTAssert(result.details == NoIndications);
+    model.susVT = YES;
+    result = [model getResult];
+    XCTAssert(result.details == NoEF);
+    model.ef = EFMoreThan35;
+    result = [model getResult];
+    XCTAssert(result.details == DetailsNone);
+    XCTAssert(result.indication == Secondary);
+    XCTAssert(result.approval == Approved);
+    XCTAssert(!result.needsSdmEncounter);
+    [self absoluteExclusionTests:model];
+    model.susVT = NO;
+    model.highRiskCondition = YES;
+    result = [model getResult];
+    XCTAssert(result.details == DetailsNone);
+    XCTAssert(result.indication == Primary);
+    XCTAssert(result.approval == Approved);
+    XCTAssert(result.needsSdmEncounter);
+    model.highRiskCondition = NO;
+    model.icdAtEri = YES;
+    // waiting period exception only occurs when there is a possible waiting period
+    model.miWithin40Days = YES;
+    result = [model getResult];
+    XCTAssert(result.details == WaitingPeriodException);
+    XCTAssert(result.indication == Other);
+    XCTAssert(result.approval == Approved);
+    XCTAssert(!result.needsSdmEncounter);
+    model.miWithin40Days = NO;
+    result = [model getResult];
+    XCTAssert(result.details == DetailsNone);
+    model.icdAtEri = NO;
+    // MADIT II indication
+    model.priorMI = YES;
+    result = [model getResult];
+    XCTAssert(result.details == NoNyha);
+    XCTAssert(result.indication == Primary);
+    XCTAssert(result.approval == ApprovalUnclear);
+    XCTAssert(!result.needsSdmEncounter);
+    model.nyha = NyhaI;
+    model.ef = EFLessThan30;
+    [self primaryPreventionApproved:model];
+    // Class IV excluded form MADIT II
+    model.nyha = NyhaIV;
+    [self primaryPreventionNotApproved:model];
+    // ScdHeft
+    model.nyha = NyhaII;
+    model.priorMI = NO;
+    model.icm = YES;
+    model.ef = EFFrom30To35;
+    [self primaryPreventionApproved:model];
+    model.nyha = NyhaIII;
+    [self primaryPreventionApproved:model];
+    model.icm = NO;
+    model.nicm = YES;
+    [self primaryPreventionApproved:model];
+    // ting prior MI shouldn't make a difference
+    model.priorMI = YES;
+    [self primaryPreventionApproved:model];
+    // clear model
+    model = [self newModel];
+    // check forgot ICM in patient with prior MI
+    model.ef = EFFrom30To35;
+    model.priorMI = YES;
+    model.nyha = NyhaII;
+    result = [model getResult];
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(result.details == ForgotIcm);
+    model.nyha = NyhaIII;
+    result = [model getResult];
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(result.details == ForgotIcm);
+    model.ef = EFMoreThan35;
+    [self primaryPreventionNotApproved:model];
+    // check forgot MI in pt with ICM
+    model = [self newModel];
+    model.ef = EFLessThan30;
+    model.icm = YES;
+    model.nyha = NyhaI;
+    result = [model getResult];
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(result.details == ForgotMI);
+    // check transplant result
+    model = [self newModel];
+    model.ef = EFLessThan30;
+    model.transplantList = YES;
+    result = [model getResult];
+    XCTAssert(result.approval == ApprovalUnclear);
+    XCTAssert(result.indication == Other);
+    XCTAssert(result.details == BridgeToTransplant);
+    [self absoluteExclusionTests:model];
+    // primary prevention overrides transplant
+    model.priorMI = YES;
+    model.nyha = NyhaI;
+    [self primaryPreventionApproved:model];
+    // but since primary prevention indication fails here, transplant result overrides
+    model.nyha = NyhaIV;
+    result = [model getResult];
+    XCTAssert(result.details == BridgeToTransplant);
+    
+}
+
+- (void)primaryPreventionApproved:(EPSCMSModel *)model {
+    struct Result result = [model getResult];
+    XCTAssert(result.details == DetailsNone);
+    XCTAssert(result.indication == Primary);
+    XCTAssert(result.approval == Approved);
+    XCTAssert(result.needsSdmEncounter);
+}
+
+- (void)primaryPreventionNotApproved:(EPSCMSModel *)model {
+    struct Result result = [model getResult];
+    XCTAssert(result.details == DetailsNone);
+    XCTAssert(result.indication == Primary);
+    XCTAssert(result.approval == NotApproved);
+    XCTAssert(!result.needsSdmEncounter);
+}
+
+
+- (void)absoluteExclusionTests:(EPSCMSModel *) model {
+    model.brainDamage = YES;
+    XCTAssert([self isAbsoluteExclusion:model]);
+    
+    // need to exit with no absolute exclusions set
+    model.brainDamage = NO;
+    XCTAssert(![self isAbsoluteExclusion:model]);
+    XCTAssert(![self isAbsoluteExclusion:model]);
+    model.cardiogenicShock = YES;
+    XCTAssert([self isAbsoluteExclusion:model]);
+    model.cardiogenicShock = NO;
+    model.nonCardiacDisease = YES;
+    XCTAssert([self isAbsoluteExclusion:model]);
+    model.uncontrolledSvt = YES;
+    XCTAssert([self isAbsoluteExclusion:model]);
+    model.nonCardiacDisease = NO;
+    XCTAssert([self isAbsoluteExclusion:model]);
+    model.uncontrolledSvt = NO;
+    XCTAssert(![self isAbsoluteExclusion:model]);
+    // no absolute exclusions  at end of this method
+
+}
+
+- (BOOL)isAbsoluteExclusion:(EPSCMSModel *) model {
+    struct Result result = [model getResult];
+    return result.details == AbsoluteExclusion;
+}
+
+- (EPSCMSModel *)newModel {
+    return [[EPSCMSModel alloc] init];
+}
 
 @end
